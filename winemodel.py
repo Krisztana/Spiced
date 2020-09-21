fb44081ef5bd11d72e6ff292d166414978f16a4f
Krisztina Namesnyik <kriszta.namesnyik@gmail.com>
2020-09-21 15:00:20 +0200

    update

--- winemodel.py
+++ winemodel.py
@@ -6,3 +6,11 @@ def train_model(X, y):
     m = DummyClassifier()
     m.fit(X, y)
     return m
+
+
+if __name__ == '__main__':
+    d = load_wine()
+    X = pd.DataFrame(d['data'], columns=d['feature_names'])
+    y = d['target']  # cultivator (0, 1, 2)
+    m = train_model(X, y)
+    m.score(X, y)
